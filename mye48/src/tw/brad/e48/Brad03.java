351d1c0571333e2bbe6a3017ab6a005dade9ad29
BradChao <brad@brad.tw>
2022-07-16 13:52:11 +0800

    init

---
+++ mye48/src/tw/brad/e48/Brad03.java
@@ -0,0 +1,11 @@
+package tw.brad.e48;
+
+public class Brad03 {
+
+	public static void main(String[] args) {
+		byte v1 = 10;
+		short v2 = 3;
+		int v3 = v1 + v2; 
+	}
+
+}
---
+++ mye48/src/tw/brad/e48/HelloWorld.java
@@ -0,0 +1,9 @@
+package tw.brad.e48;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+	}
+
+}
