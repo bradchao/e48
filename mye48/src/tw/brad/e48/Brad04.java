ac4bfc2980176bc76907553bc27353a603db918a
BradChao <brad@brad.tw>
2022-07-18 09:38:19 +0800

    07180938

--- mye48/src/tw/brad/e48/Brad04.java
+++ mye48/src/tw/brad/e48/Brad04.java
@@ -8,7 +8,20 @@ public class Brad04 {
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("x = ");
 		int x = scanner.nextInt();
-		System.out.println(x);
+		System.out.print("y = ");
+		int y = scanner.nextInt();
+		int r1 = x + y;
+		int r2 = x - y;
+		int r3 = x * y;
+		int r4 = x / y;	//
+		int r5 = x % y; 
+		
+		System.out.printf("%d + %d = %d\n", x, y, r1);
+		System.out.printf("%d - %d = %d\n", x, y, r2);
+		System.out.printf("%d x %d = %d\n", x, y, r3);
+		System.out.printf("%d / %d = %d ...... %d\n", x, y, r4, r5);
+		
+		
 	}
 
 }
