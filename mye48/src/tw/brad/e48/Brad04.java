abfa3035b78341557f8213debe30120e9c02f238
BradChao <brad@brad.tw>
2022-07-18 09:20:05 +0800

    07080920

---
+++ mye48/src/tw/brad/e48/Brad04.java
@@ -0,0 +1,14 @@
+package tw.brad.e48;
+
+import java.util.Scanner;
+
+public class Brad04 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("x = ");
+		int x = scanner.nextInt();
+		System.out.println(x);
+	}
+
+}
