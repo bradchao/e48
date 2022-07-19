a99dad119f5cb1ec40f05a6e50ac4939fcfca30a
BradChao <brad@brad.tw>
2022-07-19 11:17:04 +0800

    07191117

--- mye48/src/tw/brad/e48/PokerV3.java
+++ mye48/src/tw/brad/e48/PokerV3.java
@@ -3,7 +3,7 @@ package tw.brad.e48;
 public class PokerV3 {
 
 	public static void main(String[] args) {
-		int nums = 10;
+		int nums = 52;
 		int[] poker = new int[nums];	// index => int, > 0 
 		for (int i=0; i<poker.length; i++) poker[i] = i;
 		
@@ -14,9 +14,23 @@ public class PokerV3 {
 			poker[rand] = poker[i];
 			poker[i] = temp;
 		}
-		for(int card : poker) {
-			System.out.println(card);
+//		for(int card : poker) {
+//			System.out.println(card);
+//		}
+		
+		System.out.println("------");
+		int[][] players = new int[4][13];
+		for (int i=0; i<poker.length; i++) {
+			players[i%4][i/4] = poker[i];
 		}
+		
+		for(int[] player : players) {
+			for (int card : player) {
+				System.out.print(card + " ");
+			}
+			System.out.println();
+		}
+		
 		
 	}
 
