0c866ca8d41129f9423ad39f95447b47805cceaa
BradChao <brad@brad.tw>
2022-07-19 09:16:40 +0800

    07190916

--- mye48/src/tw/brad/e48/PokerV1.java
+++ mye48/src/tw/brad/e48/PokerV1.java
@@ -4,9 +4,27 @@ public class PokerV1 {
 
 	public static void main(String[] args) {
 		int[] poker = new int[52];
+		
 		for (int i=0; i<poker.length; i++) {
-			poker[i] = (int)(Math.random()*52);
-			System.out.println(poker[i]);
+			int temp = (int)(Math.random()*52);
+			
+			// 檢查機制
+			boolean isRepeat = false;
+			for (int j = 0; j < i; j++) {
+				if (temp == poker[j]) {
+					// 發生重複了
+					isRepeat = true;
+					break;
+				}
+			}
+			
+			if (!isRepeat) {
+				poker[i] = temp;
+				System.out.println(poker[i]);				
+			}else {
+				i--;
+			}
+			
 		}
 	}
 
