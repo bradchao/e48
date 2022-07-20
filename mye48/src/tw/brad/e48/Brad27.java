20134a5e2fb386ed1fb9185d9b0d3e777aec3768
BradChao <brad@brad.tw>
2022-07-20 10:08:11 +0800

    07201008

---
+++ mye48/src/tw/brad/e48/Brad27.java
@@ -0,0 +1,14 @@
+package tw.brad.e48;
+
+import tw.brad.utils.TWId;
+
+public class Brad27 {
+
+	public static void main(String[] args) {
+		double rand = Math.random();
+		System.out.println(rand);
+		boolean isRight = TWId.isRightId();
+		System.out.println(isRight);
+	}
+
+}
---
+++ mye48/src/tw/brad/utils/TWId.java
@@ -0,0 +1,9 @@
+package tw.brad.utils;
+
+public class TWId {
+
+	public static boolean isRightId() {
+		System.out.println("isRightId()");
+		return true;
+	}
+}
