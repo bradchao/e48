99a93512f4a8717a324c9b92b1b62979462123dd
BradChao <brad@brad.tw>
2022-07-20 12:01:01 +0800

    07201200

--- mye48/src/tw/brad/e48/Brad27.java
+++ mye48/src/tw/brad/e48/Brad27.java
@@ -5,7 +5,7 @@ import tw.brad.utils.TWId;
 public class Brad27 {
 
 	public static void main(String[] args) {
-		boolean isRight = TWId.isRightId("Y123456789");
+		boolean isRight = TWId.isRightId("A123456789");
 		System.out.println(isRight);
 		
 		
