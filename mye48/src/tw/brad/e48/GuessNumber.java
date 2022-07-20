e1e8ad514a7477b83d05ca80670377c754496c1f
BradChao <brad@brad.tw>
2022-07-20 16:11:16 +0800

    07201611

--- mye48/src/tw/brad/e48/GuessNumber.java
+++ mye48/src/tw/brad/e48/GuessNumber.java
@@ -14,6 +14,7 @@ public class GuessNumber extends JFrame implements ActionListener {
 	private JButton guess;
 	private JTextField input;
 	private JTextArea log;
+	private String answer;
 	
 	public GuessNumber() {
 		super("猜數字遊戲");
@@ -42,7 +43,26 @@ public class GuessNumber extends JFrame implements ActionListener {
 	}
 	
 	public void actionPerformed(ActionEvent e) {
-		System.out.println("OK");
+		//System.out.println("OK");
+		createAnswer(4);
+		System.out.println(answer);
+	}
+	
+	private void createAnswer(int dig) {
+		int nums = 10;
+		int[] poker = new int[nums];	// index => int, > 0 
+		for (int i=0; i<poker.length; i++) poker[i] = i;
+		
+		for (int i = nums -1; i > 0; i--) {
+			int rand = (int)(Math.random()*(i+1));
+			// poker[rand] <-> poker[i]
+			int temp = poker[rand];
+			poker[rand] = poker[i];
+			poker[i] = temp;
+		}
+		
+		answer = "" + poker[0] + poker[1] + poker[2];
+		
 	}
 
 }
