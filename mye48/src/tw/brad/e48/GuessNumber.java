65ee0fea8f8c6cdf489f60fded83ba7df75e9e7c
BradChao <brad@brad.tw>
2022-07-20 15:38:05 +0800

    07201538

--- mye48/src/tw/brad/e48/GuessNumber.java
+++ mye48/src/tw/brad/e48/GuessNumber.java
@@ -1,7 +1,10 @@
 package tw.brad.e48;
 
+import java.awt.BorderLayout;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
@@ -16,6 +19,15 @@ public class GuessNumber extends JFrame {
 		guess = new JButton("猜");
 		input = new JTextField();
 		log = new JTextArea();
+		
+		setLayout(new BorderLayout());
+		add(log, BorderLayout.CENTER);
+		
+		JPanel top = new JPanel(new BorderLayout());
+		top.add(guess, BorderLayout.EAST);
+		top.add(input, BorderLayout.CENTER);
+		
+		add(top, BorderLayout.NORTH);
 		
 		setSize(640, 480);
 		setVisible(true);
