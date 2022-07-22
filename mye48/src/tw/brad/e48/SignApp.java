0885379012bdd064644d1869761be28f5c54d650
BradChao <brad@brad.tw>
2022-07-22 10:53:03 +0800

    07221052

--- mye48/src/tw/brad/e48/SignApp.java
+++ mye48/src/tw/brad/e48/SignApp.java
@@ -1,11 +1,27 @@
 package tw.brad.e48;
 
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class SignApp extends JFrame{
+	private JButton clear, undo, redo;
 	
 	public SignApp() {
-		super("簽名App");
+		super("SignApp");
+		
+		setLayout(new BorderLayout());
+		clear = new JButton("Clear");
+		undo = new JButton("Undo");
+		redo = new JButton("Redo");
+		
+		JPanel top = new JPanel(new FlowLayout(FlowLayout.LEFT));
+		top.add(clear); top.add(undo); top.add(redo);
+		
+		add(top, BorderLayout.NORTH);
 		
 		setSize(800, 480);
 		setVisible(true);
