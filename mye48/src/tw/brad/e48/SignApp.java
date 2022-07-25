7a74cc68c15724d9e96ea0bee4ee5a9dca9653d2
BradChao <brad@brad.tw>
2022-07-25 15:51:33 +0800

    07251551

--- mye48/src/tw/brad/e48/SignApp.java
+++ mye48/src/tw/brad/e48/SignApp.java
@@ -2,6 +2,8 @@ package tw.brad.e48;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -9,7 +11,7 @@ import javax.swing.JPanel;
 
 import tw.brad.utils.MyDrawer;
 
-public class SignApp extends JFrame{
+public class SignApp extends JFrame implements ActionListener{
 	private JButton clear, undo, redo;
 	private MyDrawer myDrawer;
 	
@@ -32,6 +34,24 @@ public class SignApp extends JFrame{
 		setSize(800, 480);
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		addEventListener();
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == clear) {
+			System.out.println("Clear");
+		}else if (e.getSource() == undo) {
+			System.out.println("undo");
+		}else if (e.getSource() == redo) {
+			System.out.println("redo");
+		}
+	}
+	
+	private void addEventListener() {
+		clear.addActionListener(this);
+		undo.addActionListener(this);
+		redo.addActionListener(this);
 	}
 
 	public static void main(String[] args) {
