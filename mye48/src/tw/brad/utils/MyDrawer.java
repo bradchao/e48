dbd1a4a86d95a2c3156c5bc230e881467bbd501f
BradChao <brad@brad.tw>
2022-07-25 14:17:45 +0800

    07251417

--- mye48/src/tw/brad/utils/MyDrawer.java
+++ mye48/src/tw/brad/utils/MyDrawer.java
@@ -4,14 +4,25 @@ import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.DebugGraphics;
 import javax.swing.JPanel;
 
-public class MyDrawer extends JPanel {
+public class MyDrawer extends JPanel implements MouseListener {
 	
 	public MyDrawer() {
 		setBackground(Color.yellow);
+		
+		//addMouseListener(this);
+		
+//		MyMouseListener listener = new MyMouseListener(this);
+//		addMouseListener(listener);
+		
+		MyMouseListenerV2 listenerV2 = new MyMouseListenerV2();
+		addMouseListener(listenerV2);
+		
 	}
 
 	@Override
@@ -26,11 +37,112 @@ public class MyDrawer extends JPanel {
 		g2d.setStroke(new BasicStroke(4));
 		g2d.setColor(Color.BLUE);
 		g2d.drawLine(0, 0, 400, 200);
+	}
+	
+	private class MyMouseListenerV2 implements MouseListener {
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mousePressed(MouseEvent e) {
+			// TODO Auto-generated method stub
+			System.out.println("PressV2");
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseExited(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
 		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		System.out.println("Click");
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		System.out.println("Press");
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		System.out.println("Release");
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
 		
-		
-		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
 		
 	}
 	
 }
+
+class MyMouseListener implements MouseListener {
+	private MyDrawer myDrawer;
+	MyMouseListener(MyDrawer myDrawer){
+		this.myDrawer = myDrawer;
+	}
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		System.out.println("MyMouseListener:Press()");
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
+
+
+
+
+
