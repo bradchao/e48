b73ee68395f06e415c68777292c40b3ec8731bec
BradChao <brad@brad.tw>
2022-07-28 11:56:46 +0800

    07281156

---
+++ mye48/src/tw/brad/e48/Brad57.java
@@ -0,0 +1,35 @@
+package tw.brad.e48;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Brad57 {
+
+	public static void main(String[] args) {
+		Timer timer = new Timer();
+		MyTask m1 = new MyTask();
+		StopTask s1 = new StopTask(timer);
+		timer.schedule(m1, 3*1000);
+		timer.schedule(s1, 5*1000);
+		System.out.println("Brad");
+	}
+	
+
+}
+
+class MyTask extends TimerTask {
+	int i;
+	@Override
+	public void run() {
+		System.out.println(i++);
+	}
+}
+class StopTask extends TimerTask {
+	private Timer timer;
+	StopTask(Timer timer){this.timer = timer;}
+	@Override
+	public void run() {
+		timer.cancel();
+	}
+}
+
