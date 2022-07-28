04af4bee8d9ca5bdce6fa2e75b9643a24d73f892
BradChao <brad@brad.tw>
2022-07-28 09:15:01 +0800

    07280914

--- mye48/src/tw/brad/e48/Brad56.java
+++ mye48/src/tw/brad/e48/Brad56.java
@@ -4,8 +4,11 @@ public class Brad56 {
 	public static void main(String[] args) {
 		Brad561 b1 = new Brad561("A");
 		Brad561 b2 = new Brad561("B");
+		Brad562 b3 = new Brad562("C");
+		Thread t1 = new Thread(b3);
 		b1.start();
 		b2.start();
+		t1.start();
 		System.out.println("Brad");
 	}
 }
@@ -23,3 +26,19 @@ class Brad561 extends Thread {
 		}
 	}
 }
+
+class Brad562 implements Runnable {
+	private String name;
+	Brad562(String name){this.name = name;}
+	public void run() {
+		for (int i=0; i<20; i++) {
+			System.out.println(name + ":" + i);
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+			}
+		}
+	}
+}
+
+
