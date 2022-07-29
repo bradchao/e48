41a0ab7130611dacef1faf97861c37586e411055
BradChao <brad@brad.tw>
2022-07-29 10:55:53 +0800

    07291055

--- mye48/src/tw/brad/e48/MyGame.java
+++ mye48/src/tw/brad/e48/MyGame.java
@@ -2,7 +2,12 @@ package tw.brad.e48;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
 
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -22,8 +27,24 @@ public class MyGame extends JFrame {
 	}
 	
 	private class MyPanel extends JPanel {
+		private BufferedImage ball1, ball2;
+		
 		MyPanel(){
 			setBackground(Color.yellow);
+			try {
+				ball1 = ImageIO.read(new File("dir1/ball1.png"));
+				ball2 = ImageIO.read(new File("dir1/ball2.png"));
+			}catch(Exception e){}
+		}
+		
+		@Override
+		protected void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			Graphics2D g2d = (Graphics2D)g;
+			
+			g2d.drawImage(ball1, 100, 100, null);
+			
+			
 		}
 	}
 	
