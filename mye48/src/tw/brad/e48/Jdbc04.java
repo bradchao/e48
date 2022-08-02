0300688ebb1dd3fc1a3f1d4655307ee4ec85c159
BradChao <brad@brad.tw>
2022-08-02 14:08:04 +0800

    08021408

---
+++ mye48/src/tw/brad/e48/Jdbc04.java
@@ -0,0 +1,31 @@
+package tw.brad.e48;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+public class Jdbc04 {
+
+	public static void main(String[] args) {
+		try {
+			Properties prop = new Properties();
+			prop.put("user", "root");
+			prop.put("password", "root");
+			
+			Connection conn = DriverManager.getConnection(
+					"jdbc:mysql://localhost:3306/eeit48", prop);
+			
+			String sql = "INSERT INTO cust (cname,tel,birthday) VALUES (?,?,?)";
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			
+			
+		} catch (SQLException e) {
+			System.out.println(e.toString());
+		}
+		
+	}
+
+}
