3b54227745744fe5f3da7b736d4f5b880a27f628
BradChao <brad@brad.tw>
2022-08-02 11:45:48 +0800

    08021145

--- mye48/src/tw/brad/e48/Jdbc01.java
+++ mye48/src/tw/brad/e48/Jdbc01.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Properties;
 
 public class Jdbc01 {
 
@@ -16,8 +17,20 @@ public class Jdbc01 {
 //		}
 		
 		try {
+//			Connection conn = DriverManager.getConnection(
+//				"jdbc:mysql://localhost:3306/eeit48?user=root&password=root");
+			
+//			Connection conn = DriverManager.getConnection(
+//					"jdbc:mysql://localhost:3306/eeit48","root","root");
+
+			Properties prop = new Properties();
+			prop.put("user", "root");
+			prop.put("password", "root");
+			
 			Connection conn = DriverManager.getConnection(
-				"jdbc:mysql://localhost:3306/eeit48?user=root&password=root");
+					"jdbc:mysql://localhost:3306/eeit48", prop);
+			
+			
 			Statement stmt = conn.createStatement();
 			stmt.executeUpdate(
 				"INSERT INTO cust (cname,tel,birthday) VALUES ('brad','123','1999-01-02')");
