1336802914014b94d09b9b58aef106cf1db95041
BradChao <brad@brad.tw>
2022-08-02 10:48:47 +0800

    08021048

--- mye48/src/tw/brad/e48/Jdbc01.java
+++ mye48/src/tw/brad/e48/Jdbc01.java
@@ -1,14 +1,27 @@
 package tw.brad.e48;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Jdbc01 {
 
 	public static void main(String[] args) {
+//		try {
+//			Class.forName("com.mysql.cj.jdbc.Driver");
+//			System.out.println("OK");
+//		} catch (ClassNotFoundException e) {
+//			System.out.println(e.toString());
+//		}
+		
 		try {
-			Class.forName("brad");
+			Connection conn = DriverManager.getConnection(
+				"jdbc:mysql://localhost:3306/eeit48?user=root&password=root");
 			System.out.println("OK");
-		} catch (ClassNotFoundException e) {
+		} catch (SQLException e) {
 			System.out.println(e.toString());
 		}
+		
 	}
 
 }
