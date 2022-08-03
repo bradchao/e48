903cfff650367be586300dbebd41b4ce6cb3f9bb
BradChao <brad@brad.tw>
2022-08-03 10:47:02 +0800

    08031046

--- mye48/src/tw/brad/e48/Jdbc07.java
+++ mye48/src/tw/brad/e48/Jdbc07.java
@@ -8,6 +8,9 @@ import java.sql.SQLException;
 import java.util.Properties;
 import java.util.Scanner;
 
+import org.json.JSONStringer;
+import org.json.JSONWriter;
+
 public class Jdbc07 {
 
 	public static void main(String[] args) {
@@ -31,15 +34,27 @@ public class Jdbc07 {
 			
 			int rpp = 10;
 			int start = (page - 1)*rpp;
-			sql = String.format("SELECT * FROM souvenir WHERE addr like '澎湖%%' ORDER BY addr LIMIT %d, %d", start,rpp);
+			sql = String.format("SELECT * FROM souvenir WHERE addr like '台中%%' ORDER BY addr LIMIT %d, %d", start,rpp);
 			pstmt = conn.prepareStatement(sql);
 			rs = pstmt.executeQuery();
+			
+			JSONStringer js = new JSONStringer();
+			JSONWriter jw = js.array();
+			
 			while (rs.next()) {
 				String id = rs.getString("id");
 				String sname = rs.getString("sname");
 				String addr = rs.getString("addr");
 				System.out.println(String.format("%s:%s:%s", id,sname,addr));
+				
+				jw.object();
+				jw.key("id").value(id);
+				jw.key("name").value(sname);
+				jw.key("address").value(addr);
+				jw.endObject();
 			}
+			jw.endArray();
+			System.out.println(jw.toString());
 
 		} catch (SQLException e) {
 			System.out.println(e.toString());
