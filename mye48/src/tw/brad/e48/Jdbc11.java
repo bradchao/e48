b9d9234d665214877543c5274ff19411e4e39d3a
BradChao <brad@brad.tw>
2022-08-03 15:24:16 +0800

    08031524

--- mye48/src/tw/brad/e48/Jdbc11.java
+++ mye48/src/tw/brad/e48/Jdbc11.java
@@ -1,6 +1,7 @@
 package tw.brad.e48;
 
-import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -9,9 +10,8 @@ import java.util.Properties;
 
 import tw.brad.utils.BCrypt;
 import tw.brad.utils.Member;
-import tw.brad.utils.Student;
 
-public class Jdbc10 {
+public class Jdbc11 {
 
 	public static void main(String[] args) {
 		Properties prop = new Properties();
@@ -21,18 +21,21 @@ public class Jdbc10 {
 		try(Connection conn = DriverManager.getConnection(
 				"jdbc:mysql://localhost:3306/eeit48", prop);) {
 			
-			Student s1 = new Student(20, 30, 40, "brad");
-			FileInputStream fin = new FileInputStream("dir1/ball1.png");
-			
-			String sql = "UPDATE member SET icon  = ?, student = ? WHERE id = ?";
+			String sql = "SELECT * FROM member WHERE id = 1";
 			PreparedStatement ps = conn.prepareStatement(sql);
-			ps.setBinaryStream(1, fin);
-			ps.setObject(2, s1);
-			ps.setInt(3, 1);
-			int n = ps.executeUpdate();
-			System.out.println(n);
-			fin.close();
+			ResultSet rs = ps.executeQuery();
+			rs.next();
 			
+			FileOutputStream fout = new FileOutputStream("dir2/ball1.png");
+			InputStream in = rs.getBinaryStream("icon");
+			byte[] buf = new byte[1024];
+			int len;
+			while ( (len = in.read(buf)) != -1) {
+				fout.write(buf, 0, len);
+			}
+			fout.flush();
+			fout.close();
+			System.out.println("OK");
 		}catch(Exception e) {
 			System.out.println(e.toString());
 		}
