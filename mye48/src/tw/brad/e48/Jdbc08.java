0329ee00d674bdd690a08236da4aeacd1dad975f
BradChao <brad@brad.tw>
2022-08-03 11:37:18 +0800

    08031137

--- mye48/src/tw/brad/e48/Jdbc08.java
+++ mye48/src/tw/brad/e48/Jdbc08.java
@@ -6,11 +6,12 @@ import java.sql.PreparedStatement;
 import java.util.Properties;
 
 public class Jdbc08 {
+	static final String sqlCheckAccount = "SELECT account FROM member WHERE account = ?";
+	static final String sqlAppendAccount = "INSERT INTO member (account,passwd,realname) VALUES (?,?,?)";
+	static PreparedStatement checkStatement, appendStatement;
 
 	public static void main(String[] args) {
 		String accont = "brad", passwd = "123456", realname = "BradOK";
-		String sqlCheckAccount = "SELECT account FROM member WHERE account = ?";
-		String sqlAppendAccount = "INSERT INTO member (account,passwd,realname) VALUES (?,?,?)";
 		
 		Properties prop = new Properties();
 		prop.put("user", "root");
@@ -18,13 +19,31 @@ public class Jdbc08 {
 		
 		try(Connection conn = DriverManager.getConnection(
 				"jdbc:mysql://localhost:3306/eeit48", prop);) {
-			
+			checkStatement = conn.prepareStatement(sqlCheckAccount);
+			appendStatement = conn.prepareStatement(sqlAppendAccount);
+			if (!isDataRepeat(accont)) {
+				if (appendData(accont, passwd, realname)) {
+					System.out.println("Success");
+				}else {
+					System.out.println("E2");
+				}
+			}else {
+				System.out.println("E1");
+			}
 			
 		}catch(Exception e) {
 			System.out.println(e.toString());
 		}
 
 		
+	}
+	
+	static boolean isDataRepeat(String account) {
+		return true;
+	}
+	
+	static boolean appendData(String account, String passwd, String realname) {
+		return true;
 	}
 
 }
